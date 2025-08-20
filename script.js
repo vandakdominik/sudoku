e70a42ad3ed06e0a13da30e709c3870532f4f1ea
dominik <vandakdominik@gmail.com>
2025-08-20 15:59:10 +0200

    fixed link and add reset button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gameElem = document.querySelector("#games");
 const statusElem = document.querySelector("#sudoku-status");
 document.querySelector("#sudoku-solve").addEventListener("click", solve);
+document.querySelector("#sudoku-reset").addEventListener("click", reset);
 
 //constants used to find what numbers are in the same block.
 const sudokuSubGrid = [0, 3, 6, 27, 30, 33, 54, 57, 60]; //number of left top corners of sub grids (3*3) in main grid (9*9)
@@ -17,110 +18,134 @@ function createGrid() {
   }
 
   document.querySelector("#sudoku-game").innerHTML = gameHtml;
-  
+
   //different background colors for different subgrids
   let odd = true;
-  for(let i = 0; i < 9; i++){
-    for(let j = 0; j < 9; j++){
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
       const index = sudokuSubGrid[i] + sudokuOffset[j];
-      if(odd){
-        document.querySelector(`#sudoku-input-js-${index}`).classList.add('sudoku-odd');
-      } else{
-        document.querySelector(`#sudoku-input-js-${index}`).classList.add('sudoku-even')
-      };
-    };
-    odd ? odd = false: odd = true;
-  };
+      if (odd) {
+        document
+          .querySelector(`#sudoku-input-js-${index}`)
+          .classList.add("sudoku-odd");
+      } else {
+        document
+          .querySelector(`#sudoku-input-js-${index}`)
+          .classList.add("sudoku-even");
+      }
+    }
+    odd ? (odd = false) : (odd = true);
+  }
 
   inputControl();
 }
 createGrid();
 
-function inputControl(){ //TODO finish
-  for(let i = 0; i < 81; i++){
-    document.querySelector(`#sudoku-input-js-${i}`).addEventListener('keydown', (event)=>{movement(event, i)});
-  };
+function inputControl() {
+  //TODO finish
+  for (let i = 0; i < 81; i++) {
+    document
+      .querySelector(`#sudoku-input-js-${i}`)
+      .addEventListener("keydown", (event) => {
+        movement(event, i);
+      });
+  }
 }
 
-function movement(event, i){ //TODO finish
+function movement(event, i) {
+  //TODO finish
   const key = event.key;
   let column = i % 9;
   let row = Math.floor(i / 9);
   let index;
 
-  switch(key) {
+  switch (key) {
     //add cases for numbers to overwrite whats inside input
-    case '1':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '1';
+    case "1":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "1";
       break;
-    case '2':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '2';
+    case "2":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "2";
       break;
-    case '3':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '3';
+    case "3":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "3";
       break;
-    case '4':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '4';
+    case "4":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "4";
       break;
-    case '5':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '5';
+    case "5":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "5";
       break;
-    case '6':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '6';
+    case "6":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "6";
       break;
-    case '7':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '7';
+    case "7":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "7";
       break;
-    case '8':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '8';
+    case "8":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "8";
       break;
-    case '9':
-      document.querySelector(`#sudoku-input-js-${i}`).value = '9';
+    case "9":
+      document.querySelector(`#sudoku-input-js-${i}`).value = "9";
       break;
 
     //movment arrows + enter
-    case 'ArrowUp':
+    case "ArrowUp":
       row--;
-      if(row === -1){
+      if (row === -1) {
         row = 8;
-      };
+      }
       index = row * 9 + column;
       document.querySelector(`#sudoku-input-js-${index}`).focus();
       break;
-    case 'ArrowLeft':
+    case "ArrowLeft":
       column--;
-      if(column === -1){
+      if (column === -1) {
         column = 8;
-      };
+      }
       index = row * 9 + column;
       document.querySelector(`#sudoku-input-js-${index}`).focus();
       break;
-    case 'ArrowDown':
+    case "ArrowDown":
       index = (i + 9) % 81;
       document.querySelector(`#sudoku-input-js-${index}`).focus();
       break;
-    case 'ArrowRight':
+    case "ArrowRight":
       column = (column + 1) % 9;
       index = row * 9 + column;
       document.querySelector(`#sudoku-input-js-${index}`).focus();
       break;
-    case 'Enter':
+    case "Enter":
       index = (i + 1) % 81;
       document.querySelector(`#sudoku-input-js-${index}`).focus();
       break;
-  };
-};
+  }
+}
+
+//clears the grid
+function reset() {
+  sudokuData.fill(null);
+  updataSudoku(sudokuData);
+  //console.log(sudokuData);
+  for (let i = 0; i < 81; i++) {
+    document.querySelector(`#sudoku-input-js-${i}`).dataset.borderColor =
+      "black";
+  }
+  statusElem.innerText = "Enter values";
+}
 
 function solve() {
   statusElem.innerText = "Solving";
 
   for (let i = 0; i < 81; i++) {
     if (document.querySelector(`#sudoku-input-js-${i}`).value === "") {
-      document.querySelector(`#sudoku-input-js-${i}`).dataset.borderColor = "gray";
+      document.querySelector(`#sudoku-input-js-${i}`).dataset.borderColor =
+        "gray";
       sudokuData[i] = null;
       continue;
     } else {
-      document.querySelector(`#sudoku-input-js-${i}`).dataset.borderColor = "black";
+      document.querySelector(`#sudoku-input-js-${i}`).dataset.borderColor =
+        "black";
       const int = parseInt(
         document.querySelector(`#sudoku-input-js-${i}`).value
       );
@@ -147,42 +172,48 @@ function solve() {
 function updataSudoku(sudokuData) {
   for (let i = 0; i < 81; i++) {
     document.querySelector(`#sudoku-input-js-${i}`).value = sudokuData[i];
-  };
+  }
 }
 
-function solveAlgorithm(sudokuSolutionData, blockNumber = 0, blockIndex = -1) { //blockIndex = -1 because we increment first (with 0 we would skip first block)
-  if(blockIndex >= 8){
+function solveAlgorithm(sudokuSolutionData, blockNumber = 0, blockIndex = -1) {
+  //blockIndex = -1 because we increment first (with 0 we would skip first block)
+  if (blockIndex >= 8) {
     blockNumber++;
     blockIndex = 0;
-  } else{
+  } else {
     blockIndex++;
-  };
+  }
 
   //base case
-  if(blockNumber >= 9){
+  if (blockNumber >= 9) {
     updataSudoku(sudokuSolutionData);
     return true;
-  };
+  }
   const index = sudokuSubGrid[blockNumber] + sudokuOffset[blockIndex];
-  if(sudokuSolutionData[index] === null){
-    for(let i = 1; i <= 9; i++){
+  if (sudokuSolutionData[index] === null) {
+    for (let i = 1; i <= 9; i++) {
       //add to array if it can be there
       sudokuSolutionData[index] = i;
-      if(!checkValidityOne(sudokuSolutionData, blockNumber, blockIndex)){ //if its not valid number try other
+      if (!checkValidityOne(sudokuSolutionData, blockNumber, blockIndex)) {
+        //if its not valid number try other
         sudokuSolutionData[index] = null;
         continue;
-      };
+      }
 
-      const solved = solveAlgorithm(sudokuSolutionData, blockNumber, blockIndex);
-      if(solved){
+      const solved = solveAlgorithm(
+        sudokuSolutionData,
+        blockNumber,
+        blockIndex
+      );
+      if (solved) {
         return true;
-      };
-    };
-  } else{
+      }
+    }
+  } else {
     return solveAlgorithm(sudokuSolutionData, blockNumber, blockIndex);
-  };
-    return false;
-};
+  }
+  return false;
+}
 
 function checkValidity(sudokuData, highlightConflicts = false) {
   let valid = true;
@@ -235,8 +266,12 @@ function checkValidityOne(
 
     if (sudokuData[checkIndex] === sudokuData[index] && highlightConflicts) {
       //test
-      document.querySelector(`#sudoku-input-js-${checkIndex}`).dataset.borderColor = "red";
-      document.querySelector(`#sudoku-input-js-${checkIndex}`).dataset.borderColor = "red";
+      document.querySelector(
+        `#sudoku-input-js-${checkIndex}`
+      ).dataset.borderColor = "red";
+      document.querySelector(
+        `#sudoku-input-js-${checkIndex}`
+      ).dataset.borderColor = "red";
     }
   }
 
@@ -250,8 +285,12 @@ function checkValidityOne(
 
     if (sudokuData[checkIndex] === sudokuData[index] && highlightConflicts) {
       //test
-      document.querySelector(`#sudoku-input-js-${checkIndex}`).dataset.borderColor = "red";
-      document.querySelector(`#sudoku-input-js-${checkIndex}`).dataset.borderColor = "red";
+      document.querySelector(
+        `#sudoku-input-js-${checkIndex}`
+      ).dataset.borderColor = "red";
+      document.querySelector(
+        `#sudoku-input-js-${checkIndex}`
+      ).dataset.borderColor = "red";
     }
   }
 
@@ -265,8 +304,12 @@ function checkValidityOne(
 
     if (sudokuData[checkIndex] === sudokuData[index] && highlightConflicts) {
       //test
-      document.querySelector(`#sudoku-input-js-${checkIndex}`).dataset.borderColor = "red";
-      document.querySelector(`#sudoku-input-js-${checkIndex}`).dataset.borderColor = "red";
+      document.querySelector(
+        `#sudoku-input-js-${checkIndex}`
+      ).dataset.borderColor = "red";
+      document.querySelector(
+        `#sudoku-input-js-${checkIndex}`
+      ).dataset.borderColor = "red";
     }
   }
 
